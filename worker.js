3c07944b19fc87dfb4ab331aae728f81891e8495
SE. kelvin <71261868+kelvin-thegreat@users.noreply.github.com>
2023-12-04 21:38:22 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,11 +2,9 @@
 /* eslint-disable import/no-named-as-default */
 import { writeFile } from 'fs';
 import { promisify } from 'util';
-import Queue from 'bull/lib/queue';
+import Queue from 'bull';
 import imgThumbnail from 'image-thumbnail';
-import mongoDBCore from 'mongodb/lib/core';
 import dbClient from './utils/db';
-import Mailer from './utils/mailer';
 
 const writeFileAsync = promisify(writeFile);
 const fileQueue = new Queue('thumbnail generation');
