377411745834b93ebe1248e900259aad8edd5498
SE. kelvin <71261868+kelvin-thegreat@users.noreply.github.com>
2023-12-04 21:14:18 +0300

    Routes -> Index.js

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,24 @@
-// eslint-disable-next-line no-unused-vars
-import { Express } from 'express';
+import express from 'express';
 import AppController from '../controllers/AppController';
-import AuthController from '../controllers/AuthController';
 import UsersController from '../controllers/UsersController';
-import FilesController from '../controllers/FilesController';
-import { basicAuthenticate, xTokenAuthenticate } from '../middlewares/auth';
-import { APIError, errorResponse } from '../middlewares/error';
+import AuthController from '../controllers/AuthController';
+import FileController from '../controllers/FilesController';
 
 /**
  * Injects routes with their handlers to the given Express application.
  * @param {Express} api
  */
-const injectRoutes = (api) => {
-  api.get('/status', AppController.getStatus);
-  api.get('/stats', AppController.getStats);
-
-  api.get('/connect', basicAuthenticate, AuthController.getConnect);
-  api.get('/disconnect', xTokenAuthenticate, AuthController.getDisconnect);
-
-  api.post('/users', UsersController.postNew);
-  api.get('/users/me', xTokenAuthenticate, UsersController.getMe);
-
-  api.post('/files', xTokenAuthenticate, FilesController.postUpload);
-  api.get('/files/:id', xTokenAuthenticate, FilesController.getShow);
-  api.get('/files', xTokenAuthenticate, FilesController.getIndex);
-  api.put('/files/:id/publish', xTokenAuthenticate, FilesController.putPublish);
-  api.put('/files/:id/unpublish', xTokenAuthenticate, FilesController.putUnpublish);
-  api.get('/files/:id/data', FilesController.getFile);
-
-  api.all('*', (req, res, next) => {
-    errorResponse(new APIError(404, `Cannot ${req.method} ${req.url}`), req, res, next);
-  });
-  api.use(errorResponse);
-};
-
-export default injectRoutes;
+const route = express.Router();
+route.get('/status', ((request, response) => AppController.getStatus(request, response)));
+route.get('/stats', ((request, response) => AppController.getStats(request, response)));
+route.post('/users', ((request, response) => UsersController.postNew(request, response)));
+route.post('/files', ((request, response) => FileController.postUpload(request, response)));
+route.get('/files/:id', ((request, response) => FileController.getShow(request, response)));
+route.get('/files', ((request, response) => FileController.getIndex(request, response)));
+route.put('/files/:id/publish', ((request, response) => FileController.putPublish(request, response)));
+route.put('/files/:id/publish', ((request, response) => FileController.putUnpublish(request, response)));
+route.get('/files/:id/data', ((request, response) => FileController.getFile(request, response)));
+route.get('/connect', ((request, response) => AuthController.getConnect(request, response)));
+route.get('/disconnect', ((request, response) => AuthController.getDisconnect(request, response)));
+route.get('/users/me', ((request, response) => UsersController.getMe(request, response)));
+export default route;
